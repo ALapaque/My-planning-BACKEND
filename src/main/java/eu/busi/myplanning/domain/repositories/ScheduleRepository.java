6ccc1a0093d665e39c15b7cf5d0217151706b228
amaury.lapaque <amaury.lapaque@gmail.com>
2021-09-01 17:27:51 +0200

    need to fix Schedule entity

--- src/main/java/eu/busi/myplanning/domain/repositories/ScheduleRepository.java
+++
@@ -1,9 +0,0 @@
-package eu.busi.myplanning.domain.repositories;
-
-import eu.busi.myplanning.domain.models.Schedule;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-}
--- src/main/java/eu/busi/myplanning/domain/services/ScheduleService.java
+++
@@ -1,6 +0,0 @@
-package eu.busi.myplanning.domain.services;
-
-import eu.busi.myplanning.domain.models.Schedule;
-
-public interface ScheduleService extends GenericService<Schedule, Long> {
-}
