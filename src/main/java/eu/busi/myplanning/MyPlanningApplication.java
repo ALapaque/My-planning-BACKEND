c2c5c243dda0fc8396ada2a8428c80083ee20120
amaury.lapaque <amaury.lapaque@gmail.com>
2021-09-01 12:18:45 +0200

    first commit

---
+++ src/main/java/eu/busi/myplanning/MyPlanningApplication.java
@@ -0,0 +1,13 @@
+package eu.busi.myplanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPlanningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPlanningApplication.class, args);
+    }
+
+}
