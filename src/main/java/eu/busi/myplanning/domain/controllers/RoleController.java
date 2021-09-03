32f8d57fb408f595cec92af478a2c5ed2d81e9f0
amaury.lapaque <amaury.lapaque@gmail.com>
2021-09-03 08:06:23 +0200

    MIGRATION DONE

--- src/main/java/eu/busi/myplanning/domain/controllers/RoleController.java
+++ src/main/java/eu/busi/myplanning/domain/controllers/RoleController.java
@@ -2,9 +2,14 @@ package eu.busi.myplanning.domain.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import eu.busi.myplanning.api.RoleApi;
+import eu.busi.myplanning.domain.services.impl.RoleServiceImpl;
+import eu.busi.myplanning.exceptions.NotDeletedException;
+import eu.busi.myplanning.exceptions.NotFoundException;
+import eu.busi.myplanning.exceptions.NotSavedException;
 import eu.busi.myplanning.models.PageRoleDTO;
 import eu.busi.myplanning.models.Pageable;
 import eu.busi.myplanning.models.RoleDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +18,12 @@ import java.util.Optional;
 
 @RestController
 public class RoleController implements RoleApi {
+    private final RoleServiceImpl roleService;
+
+    public RoleController(RoleServiceImpl roleService) {
+        this.roleService = roleService;
+    }
+
     @Override
     public Optional<ObjectMapper> getObjectMapper() {
         return Optional.empty();
@@ -25,26 +36,59 @@ public class RoleController implements RoleApi {
 
     @Override
     public ResponseEntity<Boolean> deleteRole(Long id) {
-        return null;
+        try {
+            return new ResponseEntity<>(this.roleService.deleteById(id), HttpStatus.OK);
+        } catch (NotDeletedException e) {
+            log.error(e.toString());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @Override
     public ResponseEntity<RoleDTO> findRole(Long id) {
-        return null;
+        try {
+            Optional<RoleDTO> optional = this.roleService.findById(id);
+
+            if (optional.isPresent()) {
+                return new ResponseEntity<>(optional.get(), HttpStatus.OK);
+            } else {
+                throw new NotFoundException();
+            }
+        } catch (NotFoundException e) {
+            log.error(e.toString());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @Override
     public ResponseEntity<PageRoleDTO> listRoles(Pageable pageable) {
-        return null;
+        try {
+            PageRoleDTO page = new PageRoleDTO().content(this.roleService.findAll());
+
+            return new ResponseEntity<PageRoleDTO>(page, HttpStatus.OK);
+        } catch (NotFoundException e) {
+            log.error(e.toString());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @Override
     public ResponseEntity<RoleDTO> saveRole(RoleDTO body) {
-        return null;
+        try {
+            return new ResponseEntity<>(this.roleService.save(body), HttpStatus.CREATED);
+        } catch (NotSavedException e) {
+            log.error(e.toString());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @Override
     public ResponseEntity<RoleDTO> updateRole(RoleDTO body, Long id) {
-        return null;
+        try {
+            return new ResponseEntity<>(this.roleService.update(body, id), HttpStatus.CREATED);
+        } catch (NotSavedException e) {
+            log.error(e.toString());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 }
